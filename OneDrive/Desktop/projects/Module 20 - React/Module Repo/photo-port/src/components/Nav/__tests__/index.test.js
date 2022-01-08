25cde8d04956c0ab7409cf99cc6bc8cbfddcd489
Robert Isaacs <robertisaacs87@yahoo.com>
2022-01-08 14:02:30 -0600

    tests passed

--- OneDrive/Desktop/projects/Module 20 - React/Module Repo/photo-port/src/components/Nav/__tests__/index.test.js
+++ OneDrive/Desktop/projects/Module 20 - React/Module Repo/photo-port/src/components/Nav/__tests__/index.test.js
@@ -18,3 +18,20 @@ describe('Nav component', () => {
     // assert value comparison
   });
 })
+
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
+  
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+})  
+
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+      const { getByTestId } = render(<Nav />);
+      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+      expect(getByTestId('about')).toHaveTextContent('About me');
+    });
+})
--- OneDrive/Desktop/projects/Module 20 - React/Module Repo/photo-port/src/components/Nav/index.js
+++ OneDrive/Desktop/projects/Module 20 - React/Module Repo/photo-port/src/components/Nav/index.js
@@ -16,14 +16,14 @@ function Nav() {
   return (
     <header data-testid="header" className="flex-row px-1">
       <h2>
-        <a href="/">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about" onClick={() => handleClick()}>
+            <a data-testid="about" href="#about" onClick={() => handleClick()}>
               About me
             </a>
           </li>
